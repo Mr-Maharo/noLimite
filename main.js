40d85ad675561681f51bd2ad8b360374e508dc4c
Mr-Maharo <maharoless203@gmail.com>
2026-04-25 11:15:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 import { initAuth } from "./auth.js";
 import { initLobby } from "./lobby.js";
 import { enterGame } from "./game.js";
+import { tryReconnect } from "./reconnect.js";
+import { initPresence } from "./presence.js";
 
 initAuth((user) => {
 
     document.getElementById('login-screen').classList.add('hidden');
     document.getElementById('lobby-screen').classList.remove('hidden');
 
+    // 👥 presence realtime
+    initPresence(user);
+
+    // 🔁 reconnect automatique
+    tryReconnect(user, enterGame);
+
     initLobby((roomId) => {
         enterGame(roomId);
     });
-
 });
