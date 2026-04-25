6b5e6ab9657678add8145b4749fe94753a3eaaae
Mr-Maharo <maharoless203@gmail.com>
2026-04-25 08:22:57 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+import { initAuth } from "./auth.js";
+import { initLobby } from "./lobby.js";
+import { enterGame } from "./game.js";
+
+initAuth((user) => {
+
+    document.getElementById('login-screen').classList.add('hidden');
+    document.getElementById('lobby-screen').classList.remove('hidden');
+
+    initLobby((roomId) => {
+        enterGame(roomId);
+    });
+});
