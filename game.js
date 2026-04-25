6146952005c6785e3cc7524113553e1908923fe0
Mr-Maharo <maharoless203@gmail.com>
2026-04-25 08:22:35 +0200

    Create game.js

---
+++ game.js
@@ -0,0 +1,26 @@
+import { db } from "./firebase.js";
+import { doc, onSnapshot, updateDoc } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
+import { renderBoard, selectedCell } from "./board.js";
+import { botPlay } from "./bot.js";
+
+let currentRoomId = null;
+
+export function enterGame(roomId) {
+
+    currentRoomId = roomId;
+
+    onSnapshot(doc(db, "rooms", roomId), async (snap) => {
+
+        const game = snap.data();
+
+        renderBoard(game, handleMove);
+
+        if (game.turn === "bot") {
+            await botPlay(game, db, currentRoomId);
+        }
+    });
+}
+
+async function handleMove(cell, game) {
+    console.log("move...");
+}
