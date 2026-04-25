2ac231dd55da0d46b238c6f225e3aa0aa6c6b32b
Mr-Maharo <maharoless203@gmail.com>
2026-04-25 12:10:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ import {
 
 // ================= CONFIG =================
 const firebaseConfig = {
-    apiKey: "AIzaSy...",
-    authDomain: "nolimite-29e0b.firebaseapp.com",
-    projectId: "nolimite-29e0b",
+  apiKey: "AIzaSyA7ZtoI2iBifQqfiDJ-K1xrUVpxAgK77Jo",
+  authDomain: "nolimite-29e0b.firebaseapp.com",
+  databaseURL: "https://nolimite-29e0b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "nolimite-29e0b",
+  storageBucket: "nolimite-29e0b.firebasestorage.app",
+  messagingSenderId: "779663542451",
+  appId: "1:779663542451:web:e87cd9eba6d8e1bcfd88c6",
+  measurementId: "G-VZTK4QBN2J"
 };
 
 const app = initializeApp(firebaseConfig);
